8998597c1cd69125cd054600f18b07399049ac26
Thomas Manos Bajis <thomas.bajis@gmail.com>
2017-05-06 00:49:20 -0400

    feat: Set up CoreDataTableViewController.

--- Bop/BopTableViewController.swift
+++ Bop/BopTableViewController.swift
@@ -6,9 +6,47 @@
 //  Copyright © 2017 Thomas Manos Bajis. All rights reserved.
 //
 
-import Foundation
 import UIKit
+import CoreData
 
-class BopTableViewController: UITableViewController {
+// MARK: - BopTableViewController: CoreDataTableViewController
+
+class BopTableViewController: CoreDataTableViewController {
     
+    // MARK: Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set title
+        title = "Venes"
+        
+        // Create a fetchedResultsController
+        let fr = NSFetchRequest<NSFetchRequestResult>(entityName: "Pin")
+        fr.sortDescriptors = [NSSortDescriptor(key: "id", ascending: true)]
+        
+        fetchedResultsController = NSFetchedResultsController(fetchRequest: fr, managedObjectContext: AppDelegate.stack.context, sectionNameKeyPath: nil, cacheName: nil)
+    }
+    
+    // MARK: TableView Data Source
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let venue = fetchedResultsController?.object(at: indexPath) as! Pin
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PinTableCell", for: indexPath)
+        
+        cell.textLabel?.text = venue.name
+        cell.detailTextLabel?.text = "Checkin count is \(venue.checkinsCount)"
+        
+        return cell
+    }
+    
+    // MARK: Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "showDetailFromTable" {
+            let detailController = segue.destination as! BopDetailViewController
+            let venue = sender as? Pin
+            detailController.pin = venue
+        }
+    }
 }
