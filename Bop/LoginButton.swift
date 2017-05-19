7268d3fef8999751d33c4c2dc66611f79f82b8c0
Thomas Manos Bajis <thomas.bajis@gmail.com>
2017-05-18 23:09:09 -0400

    fix: Decrease brightness in LoginViewController background image. Add logo for Login View.

---
+++ Bop/LoginButton.swift
@@ -0,0 +1,32 @@
+//
+//  LoginButton.swift
+//  Bop
+//
+//  Created by Thomas Manos Bajis on 5/18/17.
+//  Copyright © 2017 Thomas Manos Bajis. All rights reserved.
+//
+
+import UIKit
+@IBDesignable
+
+// MARK: - LoginButton: UIButton
+class LoginButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            self.layer.borderWidth = borderWidth
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor = UIColor.clear {
+        didSet {
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+}
